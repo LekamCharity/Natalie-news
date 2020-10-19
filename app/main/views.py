fd30066042d19b1497149ba0f4e0827adc5d897a
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-19 17:42:56 +0300

    added an index file

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import main
+
+#Views 
+@main.route('/')
+def index():
+    '''
+    Views thats renders news sources to the home page
+    '''
+    daily_news = get_sources('general')
+    business_news = get_sources('business')
+    sport_news = get_sources('sports')
+    
+    return render_template('index.html',general = daily_news,business=business_news,sports=sport_news)
